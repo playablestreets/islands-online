8698f544deabc77374191351d139d478b235990e
jordanbinskin <jordan.binskin@gmail.com>
2021-11-16 11:54:21 +1100

    island mounds

--- src/components/Creature.js
+++ src/components/Creature.js
@@ -1,40 +1,73 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
-import ReactHowler from 'react-howler'
+import ReactHowler from "react-howler"
 
 const Wrapper = styled.div`
-    position: absolute;
-    left: ${props => `calc(${props.position.x} - ${props.dimensions.width / 2 }px)`};
-    top: ${props => `calc(${props.position.y} - ${props.dimensions.height / 2 }px)`};
-    display: ${props => props.show ? "block" : "none"};
+  position: absolute;
+  left: ${props =>
+    `calc(${props.position.x} - ${50 / 2}px)`};
+  top: ${props =>
+    `calc(${props.position.y} - ${50 / 2}px)`};
 `
 
-const Image = styled.img``
+const Image = styled.img`
+  display: ${props => (props.show ? "block" : "none")};
+  position: absolute;
+  left: ${props =>
+    `-${props.dimensions.width / 2}px`};
+  top: ${props =>
+    `-${props.dimensions.height / 2}px`};
+`
 
+const Mound = styled.div`
+  background-color: #ed8fb5;
+  border-color: 5px solid #e41052;
+  height: 50px;
+  width: 50px;
+`
 
-const Creature = ({ data, islands, index }) => {
-    const { data: d, position } = data
-    const { creature_image } = d
-    const { url, dimensions} = creature_image
-    const show = islands[index]?.show
+const Creature = ({ data }) => {
+  const { data: d, position } = data
+  const { creature_image } = d
+  const { url, dimensions } = creature_image
 
-    const [playSound, setPlaySound] = useState(false)
-    const [soundPlayer, setSoundPlayer] = useState(null)
+  const [playSound, setPlaySound] = useState(false)
+  const [soundPlayer, setSoundPlayer] = useState(null)
+  const [show, setShow] = useState(false)
 
-    useEffect(() => {
-        if (playSound) {
+  useEffect(() => {
+    if (playSound) {
+      soundPlayer.howler.fade(1, 0, 1000)
+      soundPlayer.howler.play()
+    }
+  }, [playSound])
 
-            soundPlayer.howler.fade(1, 0, 1000);
-            soundPlayer.howler.play()
-        }
-    }, [playSound])
-    
-    return (
-        <Wrapper dimensions={dimensions} onClick={() => setPlaySound(true)} position={position} show={show}>
-            <Image src={url} />
-            <ReactHowler src="/sample.wav" preload={true} playing={false} ref={(ref) => setSoundPlayer(ref)}/>
-        </Wrapper>
-    )
+  useEffect(() => {
+    if (show) {
+      setTimeout(() => {
+        setShow(false)
+      }, 5000)
+    }
+  }, [show])
+
+  return (
+    <Wrapper dimensions={dimensions} position={position}>
+      <Mound
+        onClick={() => {
+          if (show) return
+          setPlaySound(true)
+          setShow(true)
+        }}
+      />
+      <Image src={url} show={show} dimensions={dimensions} position={position} />
+      <ReactHowler
+        src="/sample.wav"
+        preload={true}
+        playing={false}
+        ref={ref => setSoundPlayer(ref)}
+      />
+    </Wrapper>
+  )
 }
 
 export default Creature
--- src/components/Island.js
+++ src/components/Island.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import styled from "styled-components"
 import Creature from "../components/Creature"
 
@@ -19,79 +19,19 @@ const Island = ({
   creatures,
 }) => {
   const { width, height } = dimensions
-  const [nextUp, setNextUp] = useState(1)
-  const [islands, updateIslands] = useState({
-    0: {
-      show: false,
-      nextUp: true,
-    },
-    1: {
-      show: false,
-      nextUp: false,
-    },
-    2: {
-      show: false,
-      nextUp: false,
-    },
-  })
 
-  const hideIsland = (islands, index) => {
-    updateIslands({
-        ...islands,
-        [index]: {
-            hideIsland: true,
-            nextUp: false,
-        }
-    })
-  }
-
-  const updateIslandState = (islands, updateIslands) => {
-    const updatedIslands = Object.fromEntries(
-      Object.entries(islands).map(([key, val], index) => {
-        if (val.nextUp) {
-          setTimeout(() => {
-              hideIsland(islands, index)
-          }, 5000)
-          return [
-            key,
-            {
-              show: true,
-              nextUp: false,
-            },
-          ]
-        } else {
-          return [
-            key,
-            {
-              show: val.show,
-              nextUp: index === nextUp % 3,
-            },
-          ]
-        }
-      })
-    )
-    updateIslands(updatedIslands)
-    setNextUp(nextUp + 1)
-  }
-
-  useEffect(() => {
-    console.log("islandState", islands)
-  }, [islands])
 
   return (
     <IslandBox
       width={width}
       height={height}
       position={position}
-      onClick={() => updateIslandState(islands, updateIslands)}
     >
       {creatures.map((creature, index) => {
         return (
           <Creature
             data={creature}
-            islands={islands}
             key={index}
-            index={index}
           />
         )
       })}
