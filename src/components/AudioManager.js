b6810d608eed4b6b839e0ae1a7674aedb8a3e042
jordanbinskin <jordan.binskin@gmail.com>
2021-11-17 16:20:46 +1100

    audio tracks - functional

--- src/components/AudioManager.js
+++ src/components/AudioManager.js
@@ -2,46 +2,37 @@ import React, { useEffect, useRef, useState } from "react"
 import ReactHowler from "react-howler"
 
 const AudioManager = ({ setPlayTrackFn }) => {
-  // useEffect(() => {
-  //     if (playSound) {
-  //       soundPlayer.howler.fade(1, 0, 1000)
-  //       soundPlayer.howler.play()
-  //     }
-  //   }, [playSound])
   const noOfTracks = 9
-
   const trackRefs = useRef([])
-
-  const [playTracks, setPlayTracks] = useState(false)
+  const playTracks = useRef(false)
 
   const playTrack = trackNo => {
-    //   console.log('from fn trackRefs', trackRefs)
-    //     trackRefs.current[trackNo]?.howler.play()
     const selectedTrack = trackRefs.current[trackNo - 1]?.howler
-    // selectedTrack.play()
-    // selectedTrack.fade(0, 1, 20000)
 
-    if (!playTracks) {
-      setPlayTracks(true)
-      trackRefs.current.forEach(track => track.play())
+    if (!playTracks.current) {
+      console.log("RUN ONCE", playTracks)
+      playTracks.current = true
+      trackRefs.current.forEach(track => track.seek(0))
     }
 
-    console.log(selectedTrack)
-      selectedTrack.fade(0, 1, 1000)
-      const start = performance.now()
-      setTimeout(() => {
-        selectedTrack.fade(1, 0, 1000)
-        const end = performance.now()
-        console.log("performance:", end - start)
-      }, 4000)
+    trackRefs.current.forEach((track, i) =>
+      console.log("track pos", i, track.seek())
+    ) // logs tracks playback time
+
+    selectedTrack.fade(0, 1, 1000)
+
+    // fades out track after 4 seconds and ends at 5 seconds
+    setTimeout(() => {
+      selectedTrack.fade(1, 0, 1000)
+    }, 4000)
   }
 
   useEffect(() => {
-    console.log("trackRefs", trackRefs)
     if (trackRefs.current.length > 0) {
       setPlayTrackFn(() => playTrack)
     }
   }, [trackRefs])
+  
 
   return (
     <>
@@ -50,7 +41,7 @@ const AudioManager = ({ setPlayTrackFn }) => {
           <ReactHowler
             src={`/static/music/Islands_Web_${i + 1}.mp3`}
             preload={true}
-            playing={false}
+            playing={true}
             ref={ref =>
               trackRefs.current.length < 9 && trackRefs.current.push(ref)
             }
@@ -58,17 +49,6 @@ const AudioManager = ({ setPlayTrackFn }) => {
             volume={0}
             loop={true}
           />
-          <button
-            style={{
-              position: "absolute",
-              zIndex: 999,
-              top: 0,
-              left: `${(i + 1) * 60}px`,
-            }}
-            onClick={() => trackRefs.current[i].howler.play()}
-          >
-            play {i}
-          </button>
         </>
       ))}
     </>
